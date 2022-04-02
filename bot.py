890d54eaa2ca807d57f2cdae84e9612787734adf
karanewh <70612794+karanewh@users.noreply.github.com>
2022-04-02 14:12:33 -0400

    Created slackbot

---
+++ bot.py
@@ -0,0 +1,21 @@
+import os
+from slack import WebClient
+from slack.errors import SlackApiError
+import feedparser
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+client = WebClient(token=slack_token)
+
+# define a variable for Eastern District Court of Pennsylvania RSS feed
+ecpa = feedparser.parse('https://ecf.paed.uscourts.gov/cgi-bin/rss_outside.pl')
+
+for entry in ecpa.entries:
+    if 'SCHOOL DISTRICT' in entry.title:
+        try:
+          response = client.chat_postMessage(
+            channel="slack-bots",
+            text=f"Update in {entry.title} \n Docket sheet: {entry.link} \n {entry.description.split(']')[0]}"
+          )
+        except SlackApiError as e:
+          # You will get a SlackApiError if "ok" is False
+          assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
